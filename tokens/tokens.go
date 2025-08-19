f995b45ea829dc8b2eeecf0ca7c4b076952e4a9a
Mandzhonson <manzhos.mihail@mail.ru>
2025-08-19 12:46:36 +0300

    add tokens int

--- tokens/tokens.go
+++ tokens/tokens.go
@@ -3,6 +3,17 @@ package tokens
 import (
 	"bufio"
 	"os"
+	"unicode"
+)
+
+const (
+	Num  = 1
+	Plus = 2
+	Minus
+	Mult = 3
+	Div
+	LParen = 4
+	RParen = 5
 )
 
 func ReadString(str *string) int {
@@ -15,4 +26,22 @@ func ReadString(str *string) int {
 		*str = line
 	}
 	return flag
+}
+
+func Tokenize(str string, arr *[]Token) {
+	rune_str := []rune(str)
+	var tokens []Token
+	i := 0
+	for i != len(rune_str) {
+		if unicode.IsDigit(rune_str[i]) {
+			j := i
+			for unicode.IsDigit(rune_str[i]) {
+				i++
+			}
+			num := []rune(rune_str[j:i])
+			tokens = append(tokens, Token{string(num), Num})
+		}
+		i++
+	}
+	*arr = tokens
 }
