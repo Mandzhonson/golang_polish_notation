ff58294069e9a1e6f7c86c468baf506d907d33db
Mandzhonson <manzhos.mihail@mail.ru>
2025-08-19 13:31:21 +0300

    need to fix paren

--- main.go
+++ main.go
@@ -9,9 +9,14 @@ func main() {
 	var list string
 	if tokens.ReadString(&list) == 1 {
 		var arr []tokens.Token
-		tokens.Tokenize(list, &arr)
-		for i := range arr {
-			fmt.Printf("%s\n", arr[i].Str)
+		if tokens.Tokenize(list, &arr) == 1 {
+			for i := range arr {
+				fmt.Printf("%s\n", arr[i].Str)
+			}
+		} else {
+			// error added
 		}
+	} else {
+		// error add
 	}
 }
--- tokens/tokens.go
+++ tokens/tokens.go
@@ -7,13 +7,13 @@ import (
 )
 
 const (
-	Num  = 1
-	Plus = 2
-	Minus
-	Mult = 3
-	Div
-	LParen = 4
-	RParen = 5
+	Num       = 1
+	PlusMinus = 2
+	MultDiv   = 3
+	LParen    = 4
+	RParen    = 5
+	Var       = 6
+	Func      = 7
 )
 
 func ReadString(str *string) int {
@@ -28,20 +28,47 @@ func ReadString(str *string) int {
 	return flag
 }
 
-func Tokenize(str string, arr *[]Token) {
+func Tokenize(str string, arr *[]Token) int {
+	flag := 1
 	rune_str := []rune(str)
-	var tokens []Token
 	i := 0
 	for i != len(rune_str) {
-		if unicode.IsDigit(rune_str[i]) {
-			j := i
+		if unicode.IsDigit(rune_str[i]) && flag == 1 {
+			start := i
 			for unicode.IsDigit(rune_str[i]) {
 				i++
 			}
-			num := []rune(rune_str[j:i])
-			tokens = append(tokens, Token{string(num), Num})
+			num := []rune(rune_str[start:i])
+			*arr = append(*arr, Token{Str: string(num), Tok: Num})
+		}
+		if (rune_str[i] == '+' || rune_str[i] == '-') && flag == 1 {
+			*arr = append(*arr, Token{Str: string(rune_str[i]), Tok: PlusMinus})
+		}
+		if (rune_str[i] == '*' || rune_str[i] == '/') && flag == 1 {
+			*arr = append(*arr, Token{Str: string(rune_str[i]), Tok: MultDiv})
+		}
+		if rune_str[i] == '(' && flag == 1 {
+			*arr = append(*arr, Token{Str: string(rune_str[i]), Tok: LParen})
+		}
+		if rune_str[i] == ')' && flag == 1 {
+			*arr = append(*arr, Token{Str: string(rune_str[i]), Tok: RParen})
+		}
+		if unicode.IsLetter(rune_str[i]) && flag == 1 {
+			start := i
+			for unicode.IsLetter(rune_str[i]) {
+				i++
+			}
+			res := string(rune_str[start:i])
+			switch res {
+			case "x":
+				*arr = append(*arr, Token{Str: res, Tok: Var})
+			case "sin", "cos", "ctg", "sqrt", "ln":
+				*arr = append(*arr, Token{Str: res, Tok: Func})
+			default:
+				flag = 0
+			}
 		}
 		i++
 	}
-	*arr = tokens
+	return flag
 }
