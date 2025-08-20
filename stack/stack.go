83426d941c482bde65d31551e04dce34181db2ea
Mandzhonson <manzhos.mihail@mail.ru>
2025-08-20 13:53:13 +0300

    stack is ready

--- stack/stack.go
+++ stack/stack.go
@@ -1,1 +1,36 @@
 package stack
+
+import "polish/tokens"
+
+type Stack struct {
+	Arr []tokens.Token
+	Top int
+}
+
+func (st *Stack) Init() {
+	st.Top = -1
+}
+
+func (st *Stack) Push(el *tokens.Token) {
+	st.Top++
+	st.Arr = append(st.Arr, *el)
+}
+
+func (st *Stack) Pop() {
+	if st.Top-1 >= 0 {
+		newSlice := make([]tokens.Token, st.Top-1)
+		copy(newSlice, st.Arr[:st.Top-1])
+		copy(st.Arr, newSlice)
+		st.Top--
+	} else {
+		st.Arr = []tokens.Token{}
+		st.Top = -1
+	}
+}
+
+func (st *Stack) Peek() tokens.Token {
+	if st.Top != -1 {
+		return st.Arr[st.Top]
+	}
+	return tokens.Token{}
+}
